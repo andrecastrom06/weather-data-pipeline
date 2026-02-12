072445de32070f29d9f33ee7a19f62181adabba9
andrecastrom06 <alcms@cesar.school>
2026-02-12 10:03:07 -0300

    Start: Silver layer medallion finished, gold layer beginning

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from medallion.bronze import bronze
 from medallion.silver import silver
+from medallion.gold import gold
 
 def main():
     print("Iniciando o processo de ETL...")
     print("Camada Bronze: Extração de dados")
     bronze()
     print("Camada Prata: Transformação de dados")
-    silver()    
+    silver()
+    print("Camada Ouro: Carregamento dos dados")
+    gold()
 
 if __name__ == "__main__":
-    bronze()
+    main()
